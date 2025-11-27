279f46447ec6f0560c663867e841636e98d3960d
marcelinombb <marcelino.m.b.braga@gmail.com>
2025-11-27 00:09:42 -0300

    testes keycloakify

--- src/login/KcPage.tsx
+++ src/login/KcPage.tsx
@@ -3,7 +3,11 @@ import type { ClassKey } from "keycloakify/login";
 import type { KcContext } from "./KcContext";
 import { useI18n } from "./i18n";
 import DefaultPage from "keycloakify/login/DefaultPage";
-import Template from "keycloakify/login/Template";
+import Template from "./Template";
+import "./main.css";
+
+const Login = lazy(() => import("./pages/Login"));
+
 const UserProfileFormFields = lazy(
     () => import("keycloakify/login/UserProfileFormFields")
 );
@@ -19,6 +23,13 @@ export default function KcPage(props: { kcContext: KcContext }) {
         <Suspense>
             {(() => {
                 switch (kcContext.pageId) {
+                    case "login.ftl": return (
+                        <Login
+                            {...{ kcContext, i18n, classes }}
+                            Template={Template}
+                            doUseDefaultCss={true}
+                        />
+                    );
                     default:
                         return (
                             <DefaultPage
@@ -37,4 +48,9 @@ export default function KcPage(props: { kcContext: KcContext }) {
     );
 }
 
-const classes = {} satisfies { [key in ClassKey]?: string };
+const classes = {
+    kcButtonClass: "",
+    kcButtonPrimaryClass: "",
+    kcButtonBlockClass: "",
+    kcButtonLargeClass: ""
+} satisfies { [key in ClassKey]?: string };
